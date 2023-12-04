5539edb09413bbb08da415d1abbcd10a89f6533b
alejoe02 <alejoespi1007@gmail.com>
2023-12-04 15:45:34 -0800

    model for tasks, functions to add and update single task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,49 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Make a project',
+      completed: true
+    },
+    {
+      id: Date.now(),
+      title: 'Make Components',
+      completed: false
+    }
+  ]);
 
+  changeHandler(event: Event) {
+
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    const newTask: string = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string){
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+  }
+  updateTask(index: number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if(position === index){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+  }
 }
